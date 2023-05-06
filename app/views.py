0f48d085b7be4cea9a8374714a22f0c8bc660ac2
preeti <preetibasyal412@gmail.com>
2023-05-06 21:44:24 +0545

    added hashed password before saving user to database

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from .models import UserPostSchema, UserLoginSchema
-from app.auth.auth_bearer import signJWT, decodeJWT
+from app.auth.auth_bearer import signJWT, decodeJWT, Hasher
 # from main import app
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
@@ -12,5 +12,6 @@ router = APIRouter()
 @router.post("/user/", tags=["User"])
 def create_user(user: UserPostSchema = Body(...)):
     user = jsonable_encoder(user)
-    new_user = user_collection.insert_one(user)
+    user["password"] = Hasher.hash_password(user.pop("password"))
+    user_collection.insert_one(user)
     return userhelper(user)
