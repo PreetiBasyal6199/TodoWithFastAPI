19d437a0e233b932572ce72d77d30ec1aa3ac496
preeti <preetibasyal412@gmail.com>
2023-05-06 22:50:49 +0545

    added views for user login

--- app/views.py
+++ app/views.py
@@ -9,9 +9,22 @@ from .helpers import userhelper
 router = APIRouter()
 
 
-@router.post("/user/", tags=["User"])
+@router.post("/user/signup/", tags=["User"])
 def create_user(user: UserPostSchema = Body(...)):
     user = jsonable_encoder(user)
     user["password"] = Hasher.hash_password(user.pop("password"))
     user_collection.insert_one(user)
     return userhelper(user)
+
+
+@router.post("/user/login/", tags=["User"])
+def login_user(user: UserLoginSchema = Body(...)):
+    user = jsonable_encoder(user)
+    current_user = user_collection.find_one({'email': user['email']})
+    if current_user:
+        if Hasher.verify_password(user['password'], current_user['password']):
+            return signJWT(current_user['email'])
+    else:
+        return {
+            "detail": "Invalid Credentials."
+        }
