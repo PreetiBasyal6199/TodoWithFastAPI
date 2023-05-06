7462463bb7f5b584a47806c8e56ad433f09e1aaa
preeti <preetibasyal412@gmail.com>
2023-05-06 20:12:48 +0545

    refactored db_connection

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from decouple import config
 from pymongo import MongoClient
 
-db_connection = MongoClient(config.get("MONGO_CONN"))
+db_connection = MongoClient(config("MONGO_CONN"))
 db_name = db_connection.TodoDatabase
 user_collection = db_name.UserCollection
 
